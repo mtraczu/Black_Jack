d2f74931fad48f1b7266f90aa196d9045f4cb8db
mtraczu <Mikolaj.traczuk@gmail.com>
2023-05-06 19:34:49 +0200

    first commit

---
+++ gry.py
@@ -0,0 +1,31 @@
+# gry
+class Player(object):
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+    def __str__(self):
+        rep = self.name + ":\t" + str(self.score)
+        return rep
+
+
+def ask_yes_no(question):
+    response = None
+    while response not in ("t", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+# if true wyswietli print, bo zostanie uruchomiony bezposrednio, musi byc false czyli modul musi zostac
+# zaimportowany.
+
+if __name__ == "__main__":
+    print("Uruchomiles ten modul bezposrednio zamiast go zaimportowac")
+    input("Nacisnij enter aby zakonczyc")
